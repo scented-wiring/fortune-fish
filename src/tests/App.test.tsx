c64bb82a49c4e7f20dd78ed123f8ba561d06ea3e
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-02-28 13:49:45 +0000

    update tests

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { fireEvent, render } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 import App from "../components/App";
 
 describe("App component", () => {
@@ -8,12 +9,14 @@ describe("App component", () => {
     expect(asFragment).toMatchSnapshot();
   });
 
-  test("renders message and hidden hand on initial load", () => {
+  test("renders message, hidden hand, hidden fish and hidden fortune on initial load", () => {
     const container = render(<App />);
     expect(
       container.getByText("Present your palm, my child.")
     ).toBeInTheDocument();
     expect(document.getElementById("hand-hide")).toBeInTheDocument();
+    expect(document.getElementById("fish-hide")).toBeInTheDocument();
+    expect(document.getElementById("fortune-hide")).toBeInTheDocument();
   });
 
   test("renders new message and hand after OK press", () => {
@@ -59,11 +62,29 @@ describe("App component", () => {
     expect(container.getByText("He comes...")).toBeInTheDocument();
   });
 
-  test("renders button after 3s", async () => {
+  test("renders fortune and try again button after a few seconds", async () => {
     const container = render(<App />);
     fireEvent.click(container.getByText("OK"));
     fireEvent.click(container.getByText("Yes"));
-    await new Promise((r) => setTimeout(r, 3000));
-    expect(container.getByRole("button")).toBeInTheDocument();
+    await act(async () => {
+      await new Promise((r) => setTimeout(r, 4800));
+    });
+    expect(document.getElementById("fortune")).toBeInTheDocument();
+    expect(container.getByRole("button").textContent).toContain("Try Again");
+  });
+
+  test("renders message on 'try again' press", async () => {
+    const container = render(<App />);
+    fireEvent.click(container.getByText("OK"));
+    fireEvent.click(container.getByText("Yes"));
+    await act(async () => {
+      await new Promise((r) => setTimeout(r, 4800));
+    });
+    fireEvent.click(container.getByText("Try Again"));
+    expect(
+      container.getByText(
+        "You can't have another go. The fish has spoken. Accept it."
+      )
+    ).toBeInTheDocument();
   });
 });
